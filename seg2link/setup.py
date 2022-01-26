45d9c756fbce9250bff5db681740ddb22a671a0c
Wen <chintou.on@gmail.com>
2022-01-26 10:43:13 +0900

    Added Requirements.txt and setup.py

---
+++ seg2link/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='seg2link',
+    version='0.1.0',
+    packages=[''],
+    package_dir={'': 'seg2link'},
+    url='https://github.com/WenChentao/Seg2Link',
+    license='MIT',
+    author='Wen',
+    author_email='Chentao Wen',
+    description='A napari based 3D segmentation software for electron microscopy images'
+)
